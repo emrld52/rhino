e66088fccb39af40726d27f15c638df1a6b655aa
emrld52 <tidepod444@gmail.com>
2026-01-25 23:08:55 +0000

    spinning cubes with basic lighting

--- src/main.c
+++ src/main.c
@@ -27,20 +27,15 @@
 float delta_time = 0.01f;
 float time;
 
-struct cam_t {
-    vec4 position;
-    vec4 desired_direction;
-    float rotation;
-    float zoom;
-};
-
-struct cam_t camera;
+float width, height;
 
 // resize gl viewport as window is resized, print debug info also
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
     printf("\nwindow resized to %dx%d", width, height);
+    width = (float)width;
+    height = (float)height;
 }
 
 // --- INPUT HANDLING --- //
@@ -48,33 +43,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 void input_handling(GLFWwindow* window) {
     // quick escape
     if(glfwGetKey(window, GLFW_KEY_ESCAPE)) glfwSetWindowShouldClose(window, true);
-
-    if(glfwGetKey(window, GLFW_KEY_W)) camera.desired_direction[1] = 1;
-    else if(glfwGetKey(window, GLFW_KEY_S)) camera.desired_direction[1] = -1;
-    else camera.desired_direction[1] = 0;
-    if(glfwGetKey(window, GLFW_KEY_D)) camera.desired_direction[0] = 1;
-    else if(glfwGetKey(window, GLFW_KEY_A)) camera.desired_direction[0] = -1;
-    else camera.desired_direction[0] = 0;
-
-    if(glfwGetKey(window, GLFW_KEY_Q)) camera.rotation += (CGLM_PI * 2) * delta_time;
-    else if(glfwGetKey(window, GLFW_KEY_E)) camera.rotation -= (CGLM_PI * 2) * delta_time;
-
-    if(glfwGetKey(window, GLFW_KEY_Z)) camera.zoom += 2 * delta_time;
-    else if(glfwGetKey(window, GLFW_KEY_X)) camera.zoom -= 2 * delta_time;
-
-    if(camera.zoom <= 0.1f) camera.zoom = 0.1f;
-
-    mat4 rotate_desired_input;
-
-    glm_mat4_identity(rotate_desired_input);
-
-    glm_rotate(rotate_desired_input, -camera.rotation, (vec3){0, 0, 1});
-    glm_mat4_mulv(rotate_desired_input, camera.desired_direction, camera.desired_direction);
-
-    // multiply by delta time
-
-    glm_vec4_mul(camera.desired_direction, (vec4){delta_time, delta_time, delta_time, 1.0f}, camera.desired_direction);
-    glm_vec4_add(camera.position, camera.desired_direction, camera.position);
 }
 
 int main(void) {
@@ -88,6 +56,9 @@ int main(void) {
     // init opengl window (fullscreen, change glfwGetPrimaryMonitor to NULL if you so need/desire windowed mode)
 
     GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Rhino Framework", NULL, NULL);
+
+    width = WINDOW_WIDTH;
+    height = WINDOW_HEIGHT;
 
     if (window == NULL) {
         printf("\nfailed to create a glfw window.");
@@ -136,23 +107,54 @@ int main(void) {
     // quad primitive as vertex coordinates
 
     float vertices[] = {
-       -0.5f,  -0.5f,   0.0f, /* bottom left */     0.0f, 0.0f,
-        0.5f,  -0.5f,   0.0f, /* bottom right */    1.0f, 0.0f,
-       -0.5f,   0.5f,   0.0f, /* top left */        0.0f, 1.0f,
-        0.5f,   0.5f,   0.0f, /* top right */       1.0f, 1.0f
-    };
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
-    unsigned int indices[] = {
-        0, 1, 2, // tri 1
-        3, 2, 1  // tri 2
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
 
     // create buffer object of gl_array_buffer type and bind to gpu with id
 
-    unsigned int VBO, EBO;
+    unsigned int VBO;
 
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
 
     // generate a vao (vertex array object)
 
@@ -165,38 +167,15 @@ int main(void) {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (GLvoid*)0);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (GLvoid*)(3 * sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
+
 
     // unbind
 
     glBindVertexArray(0);
-
-    unsigned int VBO2, EBO2, VAO2;
-
-    glGenVertexArrays(1, &VAO2);
-    glBindVertexArray(VAO2);
-
-    glGenBuffers(1, &VBO2);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO2);
-
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glGenBuffers(1, &EBO2);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO2);
-
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (GLvoid*)0);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (GLvoid*)(3 * sizeof(GLfloat)));
-    glEnableVertexAttribArray(1);
-
 
     // --- TEXTURES --- //
 
@@ -213,7 +192,7 @@ int main(void) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
     // load image with stb image
 
@@ -271,9 +250,51 @@ int main(void) {
     float last_frame_draw = 0.01f;
     float fps_timer_counter = PRINT_FRAME_TIME_PER_SECONDS;
 
-    // CGLM TEST
+    // cglm
 
-    unsigned int transformation_matrix_loc = glGetUniformLocation(shader_program, "transform");
+    // rotate quad to lay down
+
+    mat4 model;
+    glm_mat4_identity(model);
+    glm_rotate(model, glm_rad(-60.0f), (vec3){1, 0, 0});
+
+    // view matrix
+
+    mat4 view;
+    glm_mat4_identity(view);
+    glm_translate(view, (vec3){0.0f, 0.0f, -3.0f});
+
+    // projection
+
+    float fov = 60.0f;
+
+    mat4 proj;
+
+    glm_perspective(glm_rad(fov), width/height, 0.1f, 100.0f, proj);
+
+    unsigned int model_loc, view_loc, proj_loc;
+
+    model_loc = glGetUniformLocation(shader_program, "model");
+    glUniformMatrix4fv(model_loc, 1, GL_FALSE, (float*)model);
+
+    view_loc = glGetUniformLocation(shader_program, "view");
+    glUniformMatrix4fv(view_loc, 1, GL_FALSE, (float*)view);
+
+    proj_loc = glGetUniformLocation(shader_program, "projection");
+    glUniformMatrix4fv(proj_loc, 1, GL_FALSE, (float*)proj);
+
+    glEnable(GL_DEPTH_TEST);
+
+    vec3 cube_positions[20];
+
+    glm_vec3((vec4){0.0f, 0.0f, 0.0f, 1.0f}, cube_positions[0]);
+    glm_vec3((vec4){0.5f, -0.5f, -6.0f, 1.0f}, cube_positions[1]);
+    glm_vec3((vec4){5.0f, 4.5f, -10.0f, 1.0f}, cube_positions[2]);
+    glm_vec3((vec4){-5.0f, 0.5f, -12.0f, 1.0f}, cube_positions[3]);
+
+    for(int i = 1; i < 20; i++) {
+        glm_vec3((vec4){(float)(rand() % 10), (float)(rand() % 10), (float)(rand() % 10) * -1, 1.0f}, cube_positions[i]);
+    }
 
 
     // begin render loop, check input and swap buffers
@@ -283,9 +304,41 @@ int main(void) {
         // read any input, used to exit program if forced
         input_handling(window);
 
+        glUseProgram(shader_program);
+
         // set blank greenish background and clear screen
         glClearColor(0.65f, 0.8f, 1.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glm_mat4_identity(proj);
+        glm_perspective(glm_rad(fov), width/height, 0.1f, 100.0f, proj);
+        glUniformMatrix4fv(proj_loc, 1, GL_FALSE, (float*)proj);
+
+        glm_mat4_identity(view);
+        glm_translate(view, (vec3){0.0f, 0.0f, -3.0f});
+        glm_rotate(view, glm_rad(50.0f) * sin(time), (vec3){0, 0, 1});
+
+        glUniformMatrix4fv(view_loc, 1, GL_FALSE, (float*)view);
+
+        for(int i = 0; i < 20; i++) {
+            glm_mat4_identity(model);
+            glm_rotate(model, glm_rad(-60.0f * time), (vec3){0.5f, 1.0f, 0.0f});
+            glm_translate(model, cube_positions[i]);
+
+            glUniformMatrix4fv(model_loc, 1, GL_FALSE, (float*)model);
+
+            // draw quad
+
+            glBindVertexArray(VAO);
+            glUniform1i(glGetUniformLocation(shader_program, "texture_sample1"), 1);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+            glBindVertexArray(0);
+        }
+
+        // display
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
 
         // get time for shaders and also frametime counter
 
@@ -303,49 +356,6 @@ int main(void) {
             printf("\nfps : %f - frametime : %f\n", 1.0f / delta_time, delta_time);
         }
 
-        glUseProgram(shader_program);
-
-        // rebuild matrix every frame and apply position, rotation, and scale
-
-        mat4 transform_matrix;
-
-        glm_mat4_identity(transform_matrix);
-        glm_scale(transform_matrix, (vec3){camera.zoom, camera.zoom, 1.0f});
-        glm_rotate(transform_matrix, camera.rotation, (vec3){0, 0, 1});
-        glm_translate(transform_matrix, (vec3){-camera.position[0], -camera.position[1], 0.0f});
-        glm_rotate(transform_matrix, glm_rad(180.0f * time), (vec3){0, 0, 1});
-        glm_scale(transform_matrix, (vec3){0.5f, 0.5f, 1.0f});
-
-        glUniformMatrix4fv(transformation_matrix_loc, 1, GL_FALSE, (float *)transform_matrix);
-
-        // draw quad
-
-        glBindVertexArray(VAO);
-        glUniform1i(glGetUniformLocation(shader_program, "texture_sample1"), 0);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
-
-        glm_mat4_identity(transform_matrix);
-        glm_scale(transform_matrix, (vec3){camera.zoom, camera.zoom, 1.0f});
-        glm_rotate(transform_matrix, camera.rotation, (vec3){0, 0, 1});
-        glm_translate(transform_matrix, (vec3){-camera.position[0], -camera.position[1], 0.0f});
-        glm_rotate(transform_matrix, glm_rad(-180.0f * time), (vec3){0, 0, 1});
-        glm_translate(transform_matrix, (vec3){1.0f, 1.0f, 0.0f});
-        glm_rotate(transform_matrix, glm_rad(-180.0f * time), (vec3){0, 0, 1});
-        glm_scale(transform_matrix, (vec3){0.5f, 0.5f, 1.0f});
-
-        glUniformMatrix4fv(transformation_matrix_loc, 1, GL_FALSE, (float *)transform_matrix);
-
-        glBindVertexArray(VAO2);
-        glUniform1i(glGetUniformLocation(shader_program, "texture_sample1"), 0);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
-
-        // display
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-
         last_frame_draw = time;
     }
 
@@ -353,7 +363,6 @@ int main(void) {
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shader_program);
     glDeleteTextures(1, &pebbles_texture);
 
